8dfbcd5a36f0fcc2d2746730efec6a3b0a2c153a
PAOLO MIGUEL LOVINA LIBRANDO <2120740@cc.usls.edu>
2025-05-14 15:14:47 +0800

    merging

--- src/ChatMain.java
+++ src/ChatMain.java
@@ -8,7 +8,7 @@ public class ChatMain {
         ChatSec frame2 = new ChatSec();
 
         frame1.setTitle("Silay");
-        frame2.setTitle("tatlo ka talisay");
+        frame2.setTitle("tatlo ka talisay123");
         frame1.setSize(400, 230);
         frame2.setSize(400, 230);
 
